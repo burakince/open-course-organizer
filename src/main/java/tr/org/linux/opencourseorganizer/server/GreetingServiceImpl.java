47b4300f88c051b7e32ea9b3c941bbad64051e65
Burak ince <burak.ince@linux.org.tr>
2013-07-22 16:18:12 +0300

    Hermes added.

--- src/main/java/tr/org/linux/opencourseorganizer/server/GreetingServiceImpl.java
+++ src/main/java/tr/org/linux/opencourseorganizer/server/GreetingServiceImpl.java
@@ -1,48 +1,65 @@
 package tr.org.linux.opencourseorganizer.server;
 
+import java.io.IOException;
+
 import tr.org.linux.opencourseorganizer.client.GreetingService;
+import tr.org.linux.opencourseorganizer.client.Messages;
 import tr.org.linux.opencourseorganizer.shared.FieldVerifier;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.mattbertolini.hermes.Hermes;
 
 /**
  * The server side implementation of the RPC service.
  */
 @SuppressWarnings("serial")
-public class GreetingServiceImpl extends RemoteServiceServlet implements
-    GreetingService {
+public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService {
 
-  public String greetServer(String input) throws IllegalArgumentException {
-    // Verify that the input is valid.
-    if (!FieldVerifier.isValidName(input)) {
-      // If the input is not valid, throw an IllegalArgumentException back to
-      // the client.
-      throw new IllegalArgumentException(
-          "Name must be at least 4 characters long");
-    }
+	private Messages messages;
 
-    String serverInfo = getServletContext().getServerInfo();
-    String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+	public GreetingServiceImpl() {
+		try {
+			messages = Hermes.get(Messages.class, "tr");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    // Escape data from the client to avoid cross-site script vulnerabilities.
-    input = escapeHtml(input);
-    userAgent = escapeHtml(userAgent);
+	public String greetServer(String input) throws IllegalArgumentException {
+		// Verify that the input is valid.
+		if (!FieldVerifier.isValidName(input)) {
+			// If the input is not valid, throw an IllegalArgumentException back
+			// to
+			// the client.
+			throw new IllegalArgumentException(
+					"Name must be at least 4 characters long");
+		}
 
-    return "Hello, " + input + "!<br><br>I am running " + serverInfo
-        + ".<br><br>It looks like you are using:<br>" + userAgent;
-  }
+		String serverInfo = getServletContext().getServerInfo();
+		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
 
-  /**
-   * Escape an html string. Escaping data received from the client helps to
-   * prevent cross-site script vulnerabilities.
-   *
-   * @param html the html string to escape
-   * @return the escaped string
-   */
-  private String escapeHtml(String html) {
-    if (html == null) {
-      return null;
-    }
-    return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(
-        ">", "&gt;");
-  }
+		// Escape data from the client to avoid cross-site script
+		// vulnerabilities.
+		input = escapeHtml(input);
+		userAgent = escapeHtml(userAgent);
+
+		return messages.salute() + ", " + input + "!<br><br>I am running " + serverInfo
+				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+	}
+
+	/**
+	 * Escape an html string. Escaping data received from the client helps to
+	 * prevent cross-site script vulnerabilities.
+	 * 
+	 * @param html
+	 *            the html string to escape
+	 * @return the escaped string
+	 */
+	private String escapeHtml(String html) {
+		if (html == null) {
+			return null;
+		}
+		return html.replaceAll("&", "&amp;").replaceAll("<", "&lt;")
+				.replaceAll(">", "&gt;");
+	}
 }
