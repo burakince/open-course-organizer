f15ab6fa1b07c8a677203505fb3518c13874f245
Burak ince <burak.ince@linux.org.tr>
2013-07-25 01:26:17 +0300

    Desktop Client Factory added.

---
+++ src/main/java/tr/org/linux/opencourseorganizer/client/DesktopClientFactory.java
@@ -0,0 +1,11 @@
+package tr.org.linux.opencourseorganizer.client;
+
+public class DesktopClientFactory implements ClientFactory {
+
+	@Override
+	public App getApp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
