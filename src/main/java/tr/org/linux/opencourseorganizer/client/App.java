87da86d6b90f842c5e7e5db49d828ec62e2bb95e
Burak ince <burak.ince@linux.org.tr>
2013-07-25 01:15:57 +0300

    Client Factory added.

---
+++ src/main/java/tr/org/linux/opencourseorganizer/client/App.java
@@ -0,0 +1,11 @@
+package tr.org.linux.opencourseorganizer.client;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+public class App {
+
+	public void run(HasWidgets.ForIsWidget parentView) {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/main/java/tr/org/linux/opencourseorganizer/client/ClientFactory.java
@@ -0,0 +1,7 @@
+package tr.org.linux.opencourseorganizer.client;
+
+public interface ClientFactory {
+
+	public App getApp();
+
+}
