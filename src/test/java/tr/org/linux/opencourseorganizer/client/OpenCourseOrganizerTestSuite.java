d60787b3c813a1d658aa5ca0bd0f6f960639645d
Burak ince <burak.ince@linux.org.tr>
2013-07-27 18:06:54 +0300

    Test suite added.

---
+++ src/test/java/tr/org/linux/opencourseorganizer/client/OpenCourseOrganizerTestSuite.java
@@ -0,0 +1,9 @@
+package tr.org.linux.opencourseorganizer.client;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({OpenCourseOrganizerTest.class, DesktopClientFactoryTest.class})
+public class OpenCourseOrganizerTestSuite {}
