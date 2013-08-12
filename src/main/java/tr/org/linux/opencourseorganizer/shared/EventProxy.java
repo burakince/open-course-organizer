a5160c03145bd9c0585f10ff0b6bdbb91424fd8f
Burak ince <burak.ince@linux.org.tr>
2013-08-12 22:36:23 +0300

    Map RequestFactoryServlet in web.xml

--- src/main/java/tr/org/linux/opencourseorganizer/shared/EventProxy.java
+++ src/main/java/tr/org/linux/opencourseorganizer/shared/EventProxy.java
@@ -1,10 +1,12 @@
 package tr.org.linux.opencourseorganizer.shared;
 
+import tr.org.linux.opencourseorganizer.server.domain.Event;
+
 import com.google.web.bindery.requestfactory.shared.EntityProxy;
 import com.google.web.bindery.requestfactory.shared.EntityProxyId;
 import com.google.web.bindery.requestfactory.shared.ProxyFor;
 
-@ProxyFor(tr.org.linux.opencourseorganizer.server.domain.Event.class)
+@ProxyFor(Event.class)
 public interface EventProxy extends EntityProxy {
 
 	Long getId();
--- src/main/java/tr/org/linux/opencourseorganizer/shared/EventRequest.java
+++ src/main/java/tr/org/linux/opencourseorganizer/shared/EventRequest.java
@@ -2,10 +2,14 @@ package tr.org.linux.opencourseorganizer.shared;
 
 import java.util.List;
 
+import tr.org.linux.opencourseorganizer.server.domain.Event;
+
 import com.google.web.bindery.requestfactory.shared.InstanceRequest;
 import com.google.web.bindery.requestfactory.shared.Request;
 import com.google.web.bindery.requestfactory.shared.RequestContext;
+import com.google.web.bindery.requestfactory.shared.Service;
 
+@Service(Event.class)
 public interface EventRequest extends RequestContext {
 
 	Request<Long> countEvents();
