27122d1868706a4ca4a7316323356f8117ca3f60
Burak ince <burak.ince@linux.org.tr>
2013-08-12 21:42:08 +0300

    Event entity added.

---
+++ src/main/java/tr/org/linux/opencourseorganizer/server/domain/Event.java
@@ -0,0 +1,40 @@
+package tr.org.linux.opencourseorganizer.server.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Event")
+public class Event {
+
+	@Id
+	@Column(name = "Id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "Name")
+	@NotNull
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
