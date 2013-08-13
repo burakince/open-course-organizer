116676a26bc17b9096395c41558cc87ba23422ad
Burak ince <burak.ince@linux.org.tr>
2013-08-13 07:00:40 +0300

    AppServiceLocator addded.

---
+++ src/main/java/tr/org/linux/opencourseorganizer/server/domain/AppServiceLocator.java
@@ -0,0 +1,18 @@
+package tr.org.linux.opencourseorganizer.server.domain;
+
+import com.google.web.bindery.requestfactory.shared.ServiceLocator;
+
+public class AppServiceLocator implements ServiceLocator {
+
+	@Override
+	public Object getInstance(Class<?> clazz) {
+		try {
+			return clazz.newInstance();
+		} catch (InstantiationException e) {
+			throw new RuntimeException(e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
