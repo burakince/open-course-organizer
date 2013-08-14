62761077393691c72735dde9c92d8cbdcd2d7474
Burak ince <burak.ince@linux.org.tr>
2013-08-14 06:27:18 +0300

    Event dao implementation changed.

--- src/main/java/tr/org/linux/opencourseorganizer/server/dao/EventDaoImpl.java
+++ src/main/java/tr/org/linux/opencourseorganizer/server/dao/EventDaoImpl.java
@@ -1,24 +1,37 @@
 package tr.org.linux.opencourseorganizer.server.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import tr.org.linux.opencourseorganizer.server.domain.Event;
 
 public class EventDaoImpl implements EventDao {
 
+	private Event e = new Event();
+
+	public EventDaoImpl() {
+		e.setId(new Long(1));
+		e.setName("Kamp2013");
+	}
+
 	@Override
 	public Long countEvents() {
-		return null;
+		return new Long(1);
 	}
 
 	@Override
 	public List<Event> findAllEvents() {
-		return null;
+		return new ArrayList<Event>() {
+			private static final long serialVersionUID = -6775679999964806451L;
+			{
+				add(e);
+			}
+		};
 	}
 
 	@Override
 	public Event findEvent(Long id) {
-		return null;
+		return e;
 	}
 
 }
