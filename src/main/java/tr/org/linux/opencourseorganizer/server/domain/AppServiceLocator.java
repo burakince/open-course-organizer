01d80079520366628028ac96095872e70eb140c0
Burak ince <burak.ince@linux.org.tr>
2013-08-15 06:49:51 +0300

    App service locator changed.

--- src/main/java/tr/org/linux/opencourseorganizer/server/domain/AppServiceLocator.java
+++ src/main/java/tr/org/linux/opencourseorganizer/server/domain/AppServiceLocator.java
@@ -1,18 +1,21 @@
 package tr.org.linux.opencourseorganizer.server.domain;
 
+import tr.org.linux.opencourseorganizer.server.dao.EventDaoImpl;
+
 import com.google.web.bindery.requestfactory.shared.ServiceLocator;
 
 public class AppServiceLocator implements ServiceLocator {
 
 	@Override
 	public Object getInstance(Class<?> clazz) {
-		try {
-			return clazz.newInstance();
-		} catch (InstantiationException e) {
-			throw new RuntimeException(e);
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
-		}
+//		try {
+//			return clazz.newInstance();
+//		} catch (InstantiationException e) {
+//			throw new RuntimeException(e);
+//		} catch (IllegalAccessException e) {
+//			throw new RuntimeException(e);
+//		}
+		return new EventDaoImpl();
 	}
 
 }
