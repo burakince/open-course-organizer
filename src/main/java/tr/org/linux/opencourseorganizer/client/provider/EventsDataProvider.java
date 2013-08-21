594e83790f3e48f4b75ede3edc47cac996ab0257
Burak ince <burak.ince@linux.org.tr>
2013-08-21 07:10:50 +0300

    Events data provider created.

---
+++ src/main/java/tr/org/linux/opencourseorganizer/client/provider/EventsDataProvider.java
@@ -0,0 +1,16 @@
+package tr.org.linux.opencourseorganizer.client.provider;
+
+import tr.org.linux.opencourseorganizer.shared.EventProxy;
+
+import com.google.gwt.view.client.AsyncDataProvider;
+import com.google.gwt.view.client.HasData;
+
+public class EventsDataProvider extends AsyncDataProvider<EventProxy> {
+
+	@Override
+	protected void onRangeChanged(HasData<EventProxy> display) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
